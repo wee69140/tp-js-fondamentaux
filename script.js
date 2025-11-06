a76fb3d388dde0ca2eabaa554674b54f5f7cc252
weee69140 <virone.mohamed@gmail.com>
2025-11-06 14:57:14 +0000

    feat: Maîtrise des Tableaux et Boucles for

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 console.log("Laboratoire prêt !");
 const nom = "mohamed";
 let age = 17;
+
+
 const motDePasseAttendu = "secret123";
 let motDePasseUtilisateur = "12bb5"; //
+
+
 if (motDePasseUtilisateur===motDePasseAttendu) {
     console.log("Accès autorisé. Bienvenue !");
 } else if (motDePasseUtilisateur.length<8) {
@@ -10,3 +14,24 @@ if (motDePasseUtilisateur===motDePasseAttendu) {
 } else {
     console.log("Accès refusé. Mot de passe incorrect.");
 }
+
+
+const moi = {
+    prenom: "azz69",
+    nom: "virone",
+    age: 17,
+    competences: ["HTML", "CSS"]
+};
+
+console.log(moi);
+console.log("Je m'appelle azz69 virone.")
+const notes = [12, 15, 9, 18];
+console.log(notes[0]);
+console.log(notes.length);
+for (let i = 0; i < notes.length; i++) {
+    console.log("La note à l'indice " + i + " est " + notes[i]);
+}
+const classe = [
+    { prenom: 'Alice', age: 18 },
+    { prenom: 'Bob', age: 17 }
+];
